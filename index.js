cf03a83031fe7206c13a3729bebdc3ed956201b8
JacobsCounsel <admin@jacobscounsellaw.com>
2025-08-25 10:50:09 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1598,6 +1598,7 @@ function generateClientConfirmationEmail(formData, price, submissionType, leadSc
   const concerns = formData.q7 || formData.answers?.q7 || [];
   
   return `
+  }
 <!DOCTYPE html>
 <html>
 <head>
